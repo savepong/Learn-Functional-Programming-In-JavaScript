948f49d1da47f9d0a0869f21a147afe3defce2a2
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2020-05-10 20:29:17 +0700

    Lean What is Higher-Order Functions

--- EP.2 - What is Higher-Order Functions/index.js
+++ EP.2 - What is Higher-Order Functions/index.js
@@ -1,3 +1,4 @@
+// EP2
 const persons = [
   { name: "John", age: 17 },
   { name: "Jane", age: 10 },
@@ -47,3 +48,51 @@ console.log("Older persons: ", olderPersons);
 // With Higher-Order Functions
 const totalAge = persons.reduce((age, person) => age + person.age, 0);
 console.log("Total age: ", totalAge);
+
+// EP3
+// ForEach
+// Without Higher-Order Functions
+// for (let i = 0; i < persons.length; i++) {
+//   const person = persons[i];
+//   if (person.age > 15) continue;
+//   console.log(`Name ${person.name}, age ${person.age}`);
+// }
+
+// With Higher-Order Functions
+persons.forEach((person) =>
+  console.log(`Name ${person.name}, age ${person.age}`)
+);
+
+// Find, FindIndex
+// Without Higher-Order Functions
+// let jane;
+// for (let i = 0; i < persons.length; i++) {
+//   const person = persons[i];
+//   if (person.name === "Jane") {
+//     jane = person;
+//     break;
+//   }
+// }
+// console.log("Jane: ", jane);
+
+// With Higher-Order Functions
+const jane = persons.find((person) => person.name === "Jane");
+const janeIndex = persons.findIndex((person) => person.name === "Jane");
+console.log("Jane: ", jane);
+console.log("Jane index: ", janeIndex);
+
+// Every, some
+// Without Higher-Order Functions
+// let isKid = true;
+// for (let i = 0; i < persons.length; i++) {
+//   const person = persons[i];
+//   if (person.age > 15) {
+//     isKid = false;
+//     break;
+//   }
+// }
+
+// With Higher-Order Functions
+// const isKid = persons.every((person) => person.age <= 15);
+const isKid = persons.some((person) => person.age <= 15);
+console.log("isKid: ", isKid);
--- README.md
+++ README.md
@@ -2,3 +2,49 @@
 
 1. [Functional Programming คืออะไร](https://www.youtube.com/watch?v=-iNKn5eBIak&list=PLOgiLP3tCaPUDsXEB-3dGGO3oxGDRMmQe&index=2)
 2. [Higher-Order Functions คืออะไร (1/2)](https://www.youtube.com/watch?v=kapV19HYHzE&list=PLOgiLP3tCaPUDsXEB-3dGGO3oxGDRMmQe&index=3)
+
+```JavaScript
+const persons = [
+  { name: "John", age: 17 },
+  { name: "Jane", age: 10 },
+  { name: "Jim", age: 15 },
+];
+
+const kids = persons.filter((person) => person.age <= 15);
+// kids:  (2) [{…}, {…}]
+
+const olderPersons = persons.map((person) => ({
+  ...person,
+  age: person.age * 2,
+}));
+// olderPersons:  (3) [{…}, {…}, {…}]
+
+const totalAge = persons.reduce((age, person) => age + person.age, 0);
+// totalAge: 42
+```
+
+2. [Higher-Order Functions คืออะไร (2/2)](https://www.youtube.com/watch?v=pEFbMsogLdo&list=PLOgiLP3tCaPUDsXEB-3dGGO3oxGDRMmQe&index=4)
+
+```JavaScript
+const persons = [
+  { name: "John", age: 17 },
+  { name: "Jane", age: 10 },
+  { name: "Jim", age: 15 },
+];
+
+persons.forEach((person) =>
+  console.log(`Name ${person.name}, age ${person.age}`)
+);
+// Name John, age 17
+// Name Jane, age 10
+// Name Jim, age 15
+
+const jane = persons.find((person) => person.name === "Jane");
+// jane:  {name: "Jane", age: 10}
+
+const janeIndex = persons.findIndex((person) => person.name === "Jane");
+// janeIndex:  1
+
+const isKid = persons.some((person) => person.age <= 15);
+// isKid:  false
+```
